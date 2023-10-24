d0ced6fb180086fb65c2f71088e3a066ea8f4746
Marco Solari <m.solari@se20.qmul.ac.uk>
2023-10-24 20:09:10 +0100

    Enemy power pop up added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,17 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
+using TMPro;
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private int power;
 
+    [SerializeField]
+    private GameObject FloatingTextPrefab;
+
+
+    public void Start()
+    {
+        this.FloatingTextPrefab.transform.position = new Vector3(this.transform.position.x, 4, this.transform.position.z); 
+        
+    }
     public void TakeDamage(int amount)
     {
-       this.power -= amount;
-       if (this.power <= 0)
+        this.power -= amount;
+        if (this.power <= 0)
         {
             Die();
         }
@@ -22,4 +32,19 @@ public class Enemy : MonoBehaviour
         Destroy(this.gameObject);
         Debug.Log("Enemy Destroyed");
     }
+
+    private void OnMouseOver()
+    {
+    
+      this.FloatingTextPrefab.SetActive(true);
+      this.FloatingTextPrefab.GetComponent<TextMesh>().text = this.power.ToString();
+      this.FloatingTextPrefab.transform.LookAt(FloatingTextPrefab.transform.position - Camera.main.transform.position);
+  
+
+    }
+    private void OnMouseExit()
+    {
+        this.FloatingTextPrefab.SetActive(false);
+    }
+
 }
